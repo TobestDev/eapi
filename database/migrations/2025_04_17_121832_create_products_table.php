194f32cde05976d60660d9a33937c4cb15b5ec04
TobestDev <100445820+TobestDev@users.noreply.github.com>
2025-04-17 14:40:59 +0100

    Seed commit

--- database/migrations/2025_04_17_121832_create_products_table.php
+++ database/migrations/2025_04_17_121832_create_products_table.php
@@ -12,7 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('details')->nullable();
+            $table->decimal('price', 10, 2)->unsigned();
+            $table->unsignedInteger('stock')->default(0);
+            $table->unsignedInteger('discount')->default(0);
             $table->timestamps();
         });
     }
